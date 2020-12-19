84bce6ab2210cd13fcdfebe9f130eccfef124fa9
erick-pixel <erickapriansyah8@gmail.com>
2020-12-19 19:46:20 +0700

    detail

--- app/src/main/java/com/example/fani/DetailActivity.java
+++ app/src/main/java/com/example/fani/DetailActivity.java
@@ -3,6 +3,10 @@ package com.example.fani;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView judul = findViewById(R.id.judul);
+        TextView type = findViewById(R.id.type);
+        TextView episode = findViewById(R.id.episode);
+        TextView start = findViewById(R.id.start);
+        TextView end = findViewById(R.id.end);
+        TextView sinopsis = findViewById(R.id.sinopsis);
+        ImageView gambar = findViewById(R.id.gambar);
+
+        judul.setText(getIntent().getStringExtra("judul"));
+        type.setText("Type: "+getIntent().getStringExtra("type"));
+        episode.setText("Jumlah Episode: "+getIntent().getStringExtra("episode"));
+        start.setText("Tanggal Mulai: "+getIntent().getStringExtra("start"));
+        end.setText("Tanggal Berakhir: "+getIntent().getStringExtra("end"));
+        sinopsis.setText(getIntent().getStringExtra("sinopsis"));
+       Glide.with(getApplicationContext())
+                .load(getIntent()
+                        .getStringExtra("gambar")).error(R.mipmap.logo).into(gambar);
     }
 }
